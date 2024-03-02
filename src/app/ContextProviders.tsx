c294cb62c6b5344c72c910fed31236ed98a145a1
milahntg <milahn@teamgeek.io>
2024-03-02 15:19:20 +0200

    feat(landing):Added Grid Landing

--- src/app/ContextProviders.tsx
+++ src/app/ContextProviders.tsx
@@ -1,11 +1,16 @@
-"use client";
+'use client'
 
-import React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { ThemeProviderProps } from 'next-themes/dist/types'
+import { ThemeProvider } from 'next-themes'
 type Props = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
+
+export function ThemeProviderProps({ children }: Props) {
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>
+}
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,10 +19,15 @@ const queryClient = new QueryClient({
       retry: false,
     },
   },
-});
+})
+const QueryProvider = ({ children }: Props) => {
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+}
 
-export const ContextProviders: React.FC<Props> = ({ children }) => {
+export const ContextProviders = ({ children }: Props) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
-};
+    <QueryProvider>
+      <ThemeProviderProps>{children}</ThemeProviderProps>
+    </QueryProvider>
+  )
+}
