febdb58439676412474b16ce3718bce1e4dc750e
milahntg <milahn@teamgeek.io>
2024-05-07 22:51:31 +0200

    fix: end wording

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,10 +8,10 @@ function Hero() {
           LS PLANNER
         </p>
         <p className="text-start bg-clip-text text-6xl text-transparent drop-shadow-2xl bg-gradient-to-b from-white/20 to-white/20 leading-tight">
-          Bringing an
+          Bringing an{' '}
           <span className="bg-clip-text font-bold text-transparent drop-shadow-xl bg-gradient-to-r from-purple-600 to-red-500 tracking-wide">
             end
-          </span>
+          </span>{' '}
           to Loadshedding
         </p>
       </div>
